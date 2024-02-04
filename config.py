506d37f597abc33cb3d10df5aac63d58f8036cd6
Avdushin <avdushinbeaver1@gmail.com>
2024-02-04 06:59:13 +0300

    Try to Ci

--- config.py
+++ config.py
@@ -4,4 +4,12 @@ import os
 # Find .env file
 load_dotenv(find_dotenv())
 
+# Получаем значение API_TOKEN
 API_TOKEN = os.getenv('API_TOKEN')
+
+# Если .env файл отсутствует, используем переменную окружения API_TOKEN
+if API_TOKEN is None:
+    API_TOKEN = os.environ.get('API_TOKEN')
+
+if API_TOKEN is None:
+    raise ValueError("API_TOKEN is not set. Create a .env file or set it as an environment variable.")
